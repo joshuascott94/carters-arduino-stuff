362150591d6edc83d3762faa3c39a2a5397b1707
Joshua Scott <joshua@joshuascott.net>
2023-01-22 09:30:43 -0800

    Update solardevice.ino

--- solardevice.ino
+++ solardevice.ino
@@ -1,39 +1,46 @@
-/*
-  keyestudio sun_follower
-  lesson 11
-  sun_follower
-  http://www.keyestudio.com
-*/
+// Include Wire for...
 #include <Wire.h>
+
+// Include LCD display driver and configure I2C
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #include <BH1750.h>
 BH1750 lightMeter;
 
-#include <dht11.h>  //include the library code:
+// include the library code:
+// define the DHT11 as the digital pin 7
+#include <dht11.h>      
 dht11 DHT;
-#define DHT11_PIN 7  //define the DHT11 as the digital pin 7
+#define DHT11_PIN 7     
 
+// define the name of the servo rotating right and left
+// define the name of the servo rotating upwards and downwards
 #include <Servo.h>
-Servo lr_servo;  // define the name of the servo rotating right and left
-Servo ud_servo;  // define the name of the servo rotating upwards and downwards
+Servo lr_servo;         
+Servo ud_servo;
 
-const byte buttonPin = 2;  //the pin of button;the corruption is disrupted
-long buttonTimer = 0;
-long longPressTime = 250;
-boolean buttonActive = false;
-boolean longPressActive = false;
+ // set button to pin 2;
+const byte buttonPin = 2;           
 
-int lr_angle = 90;  //set the initial angle to 90 degree
-int ud_angle = 10;  //set the initial angle to 10 degree;keep the solar panels upright to detect the strongest light
-int l_state = A0;   //define the analog voltage input of the photoresistors
+// set the initial angle to 90 degree
+// set the initial angle to 10 degree; keep the solar panels upright to detect the strongest light
+// define the analog voltage input of the photoresistors
+int lr_angle = 90;                  
+int ud_angle = 10;  
+int l_state = A0;   
 int r_state = A1;
 int u_state = A2;
 int d_state = A3;
-const byte buzzer = 6;        //set the pin of the buzzer to digital pin 6
-const byte lr_servopin = 9;   //define the control signal pin of the servo rotating right and lef
-const byte ud_servopin = 10;  //define the control signal pin of the servo rotating clockwise and anticlockwise
+
+//set the pin of the buzzer to digital pin 6
+const byte buzzer = 6;
+
+//define the control signal pin of the servo rotating right and lef
+const byte lr_servopin = 9;   
+
+//define the control signal pin of the servo rotating clockwise and anticlockwise
+const byte ud_servopin = 10;  
 
 unsigned int light;   //save the variable of light intensity
 byte error = 10;      //Define the error range to prevent vibration
@@ -43,43 +50,63 @@ int temperature;      //save the variable of temperature
 int humidity;         //save the variable of humidity
 
 void setup() {
-  Serial.begin(9600);  //define the serial baud rate
+  //define the serial baud rate
+  Serial.begin(9600);  
+
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin();
   lightMeter.begin();
 
-  lr_servo.attach(lr_servopin);  // set the control pin of servo
-  ud_servo.attach(ud_servopin);  // set the control pin of servo
-  pinMode(l_state, INPUT);       //set the mode of pin
+  // set the control pin of left-right servo
+  lr_servo.attach(lr_servopin);
+
+  // set the control pin of up-down servo
+  ud_servo.attach(ud_servopin);  
+
+  // set the mode of servo pins
+  pinMode(l_state, INPUT);       
   pinMode(r_state, INPUT);
   pinMode(u_state, INPUT);
   pinMode(d_state, INPUT);
 
-  pinMode(buttonPin, INPUT_PULLUP);                                               //the button pin is set to input pull-up mode
-  attachInterrupt(digitalPinToInterrupt(buttonPin), adjust_resolution, CHANGE);  //xternal interrupt touch type is falling edge; adjust_resolution is interrupt service function ISR
+  // set the button pin is set to input pull-up mode
+  // and attach external interrupt touch type is falling edge; adjust_resolution is interrupt service function ISR    
+  pinMode(buttonPin, INPUT_PULLUP);         
+  attachInterrupt(digitalPinToInterrupt(buttonPin), adjust_resolution, FALLING);  
 
-  lcd.init();       // initialize the LCD
-  lcd.backlight();  //set LCD backlight
+   // initialize the LCD and set LCD backlight
+  lcd.init();      
+  lcd.backlight();  
 
-  lr_servo.write(lr_angle);  //return to initial angle
+  //return to initial angle
+  lr_servo.write(lr_angle);  
   delay(1000);
   ud_servo.write(ud_angle);
   delay(1000);
 }
 
 void loop() {
-  ServoAction();   //servo performs the action
-  read_light();    //read the light intensity of bh1750
-  read_dht11();    //read the value of temperature and humidity
-  LcdShowValue();  //Lcd shows the values of light intensity, temperature and humidity
+  //servo performs the action
+  ServoAction();
+
+  //read the light intensity of bh1750
+  read_light();
+
+  //read the value of temperature and humidity
+  read_dht11();
+
+  //Lcd shows the values of light intensity, temperature and humidity
+  LcdShowValue();  
 }
 
 /**********the function of the servo************/
 void ServoAction() {
-  int L = analogRead(l_state);  //read the analog voltage value of the sensor, 0-1023
+  //read the analog voltage value of the sensor, 0-1023
+  int L = analogRead(l_state);  
   int R = analogRead(r_state);
   int U = analogRead(u_state);
   int D = analogRead(d_state);
+
   /**********************system adjusting left and right序**********************/
   //  abs() is the absolute value function
   if (abs(L - R) > error && L > R) {  //Determine whether the error is within the acceptable range, otherwise adjust the steering gear
@@ -149,11 +176,14 @@ void LcdShowValue() {
   char str1[5];
   char str2[2];
   char str3[2];
-  dtostrf(light, -5, 0, str1);  //Format the light value data as a string, left-aligned
+
+  // Format the light value data as a string, left-aligned
+  dtostrf(light, -5, 0, str1);  
   dtostrf(temperature, -2, 0, str2);
   dtostrf(humidity, -2, 0, str3);
-  //LCD1602 display
-  //display the value of the light intensity
+
+  // LCD1602 display
+  // display the value of the light intensity
   lcd.setCursor(0, 0);
   lcd.print("Light:");
   lcd.setCursor(6, 0);
@@ -161,7 +191,7 @@ void LcdShowValue() {
   lcd.setCursor(11, 0);
   lcd.print("lux");
 
-  //display the value of temperature and humidity
+  // display the value of temperature and humidity
   lcd.setCursor(0, 1);
   lcd.print((int)round(1.8 * temperature + 32));
   lcd.setCursor(2, 1);
@@ -172,10 +202,10 @@ void LcdShowValue() {
   lcd.print("%");
 
   //show the accuracy of rotation
-  lcd.setCursor(11, 1);
-  lcd.print("res:");
-  lcd.setCursor(15, 1);
-  lcd.print(resolution);
+  // lcd.setCursor(11, 1);
+  // lcd.print("res:");
+  // lcd.setCursor(15, 1);
+  // lcd.print(resolution);
   /*if (light < 10) {
     lcd.setCursor(7, 0);
     lcd.print("        ");
@@ -205,18 +235,22 @@ void LcdShowValue() {
 }
 
 void read_light() {
-  light = lightMeter.readLightLevel();  //read the light intensity detected by BH1750
+  //read the light intensity detected by BH1750
+  light = lightMeter.readLightLevel();  
 }
 
 void read_dht11() {
   int chk;
-  chk = DHT.read(DHT11_PIN);  // read data
+  // read data
+  chk = DHT.read(DHT11_PIN);  
   switch (chk) {
     case DHTLIB_OK:
       break;
-    case DHTLIB_ERROR_CHECKSUM:  //check and return error
+    //check and return error
+    case DHTLIB_ERROR_CHECKSUM:
       break;
-    case DHTLIB_ERROR_TIMEOUT:  //Timeout and return error
+     //Timeout and return error
+    case DHTLIB_ERROR_TIMEOUT: 
       break;
     default:
       break;
@@ -225,45 +259,18 @@ void read_dht11() {
   humidity = DHT.humidity;
 }
 
-/*********function disrupts service**************/
+/********* function disrupts service **************/
 void adjust_resolution() {
-  if (digitalRead(buttonPin) == HIGH) {
-
-		if (buttonActive == false) {
-			buttonActive = true;
-			buttonTimer = millis();
-		}
-
-		if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
-			longPressActive = true;
-      play_song();
-		}
-
-	} else {
-		if (buttonActive == true) {
-			if (longPressActive == true) {
-				longPressActive = false;
-			} else {
-        if (resolution < 5) {
-          resolution++;
-        } else {
-          resolution = 1;
-        }
-			}
-
-			buttonActive = false;
-		}
-
-	}
-
-  tone(buzzer, 1000, 100);
+  // tone(buzzer, 1000, 100);
   delay(10);  //delay to eliminate vibration
-  if (!digitalRead(buttonPin)) {
-    if (resolution < 5) {
+  if (!digitalRead(interruptPin)) {
+    play_song();
+    /* if (resolution < 5) {
       resolution++;
     } else {
       resolution = 1;
     }
+    */
   }
 }
 
@@ -367,6 +374,7 @@ void play_song() {
     NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_Bb3, NOTE_A3,
     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4, NOTE_Bb3,
     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4, NOTE_Bb3,
+
     NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_Bb3, NOTE_A3, REST,
     NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_Bb3, NOTE_A3,
     NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_C4, NOTE_Bb3,
@@ -376,6 +384,7 @@ void play_song() {
     NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_F4,
     NOTE_Bb3, NOTE_Bb3, NOTE_Bb3, NOTE_Bb3, NOTE_Bb3, NOTE_Bb3,
     NOTE_C4, NOTE_Bb3, NOTE_A3, NOTE_Bb3, NOTE_C4, NOTE_F4,
+
     NOTE_G3, NOTE_G3, NOTE_G3, NOTE_G3, NOTE_G3, NOTE_G3,
     NOTE_A3, NOTE_G3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_A3,
     NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3,
@@ -384,6 +393,7 @@ void play_song() {
     NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_Bb3, NOTE_A3,
     NOTE_C4, NOTE_D4, NOTE_F4, NOTE_F4, NOTE_D4, NOTE_F4, NOTE_D4, NOTE_F4, NOTE_G4, NOTE_A4,
     REST, 
+
     NOTE_A4, NOTE_G4, NOTE_F4, NOTE_D4,
     NOTE_F4, NOTE_G4, NOTE_F4, NOTE_F4,
     NOTE_G4, NOTE_A4, NOTE_C5, NOTE_C5,
